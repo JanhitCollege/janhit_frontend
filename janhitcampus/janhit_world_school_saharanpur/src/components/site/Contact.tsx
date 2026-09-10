f0b42fcd0dfc0b2646a6842341c8dabb187bf352
Intexa <theintexa@gmail.com>
2026-09-10 16:24:52 +0530

    fix saharanpur

--- janhitcampus/janhit_world_school_saharanpur/src/components/site/Contact.tsx
+++ janhitcampus/janhit_world_school_saharanpur/src/components/site/Contact.tsx
@@ -186,7 +186,7 @@ export function Contact() {
                     WhatsApp Chat
                   </div>
                   <div className="font-serif text-sm text-navy font-semibold mt-0.5 whitespace-nowrap">
-                    +91 99585 74400
+                    +91 99585 
                   </div>
                 </div>
               </a>
